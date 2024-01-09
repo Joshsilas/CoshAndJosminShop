c15960889e74492324c1b5bcedd8182bcdd6830c
Joshsilas <137796554+Joshsilas@users.noreply.github.com>
2024-01-09 15:07:06 +0000

    Naughty Josh made a component in the story1 branch
    Made a single componenet to practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from "./components/Navbar/index.jsx";
 
 function App() {
 
 
   return (
     <>
+        <Navbar />
+
         <p>Hello Cosmin I have set up a repo</p>
     </>
   )
---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,14 @@
+import './NavBar.css'
+
+const NavBar = () => {
+
+    return (
+        <div>
+            <nav className="navBar">
+
+            </nav>
+        </div>
+    )
+}
+
+export default NavBar
