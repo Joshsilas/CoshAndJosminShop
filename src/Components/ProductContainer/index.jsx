b1b0b7a504cb7c770dbdb037052265f118680989
Joshsilas <137796554+Joshsilas@users.noreply.github.com>
2024-01-11 15:40:52 +0000

    Found the div that was causing issues

--- src/Components/ProductContainer/index.jsx
+++ src/Components/ProductContainer/index.jsx
@@ -2,13 +2,11 @@ import './ProductContainer.css'
 const ProductContainer = ({id, category, title, price, image, description}) => {
     return (
         <div>
-        <div className='single-product'>
         <img src={image} alt={image} className='img'/>
          <div className='product-price'>£{price}</div>
          <h2>{title}</h2>
          <p>{category.toUpperCase()}</p>
          <p>{description}</p>
-        </div>
             </div>
     )
 }
