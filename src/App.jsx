092a9f06df305d94020d20f0748bbbb0082103cf
CosminDarius1 <83471623+CosminDarius1@users.noreply.github.com>
2024-01-11 18:39:56 +0000

    Deleted unnecessary imports inside the App Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import './App.css'
 import Navbar from "./Components/Navbar/index.jsx";
-import CategoryMenu from "./Components/Category Menu/index.jsx";
-import SearchBar from "./Components/Search Bar/index.jsx";
-import CartButton from "./Components/Cart Button/index.jsx";
 
 function App() {
 
