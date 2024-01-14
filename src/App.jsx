6b50037fac28f2331a75a3cd74516ce48a6b3e0e
CosminDarius1 <83471623+CosminDarius1@users.noreply.github.com>
2024-01-14 15:18:57 +0000

    Created Product New Page, created routing and a new fetch request for only 1 item, still not working properly yet

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,26 @@ import './App.css'
 import Navbar from "./Components/Navbar/index.jsx";
 import Product from "./Components/Product/index.jsx";
 import Footer from "./Components/Footer/index.jsx";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import ProductPage from "./Components/ProductPage/index.jsx";
 
 function App() {
   return (
-    <>
-
-        <Navbar />
-        <div className = 'banner'>
-            <p>Welcome to Cosh and Josmin. The pretend E Shop!</p>
-            <p>See whats hot!</p>
-        </div>
+    <BrowserRouter>
+        <Navbar/>
+             <div className = 'banner'>
+                 <p>Welcome to Cosh and Josmin. The pretend E Shop!</p>
+                 <p>See whats hot!</p>
+             </div>
         <main className='main'>
             <Product />
         </main>
-        <Footer />
-    </>
+        <Routes>
+            <Route path='/' element={<main className='main'><Product /></main>}/>
+            <Route path='/product-page/:id' element={<ProductPage/>}/>
+        </Routes>
+            <Footer />
+    </BrowserRouter>
 
   )
 }
--- src/Components/ProductContainer/index.jsx
+++ src/Components/ProductContainer/index.jsx
@@ -1,8 +1,19 @@
 import './ProductContainer.css'
-const ProductContainer = ({id, category, title, price, image, description}) => {
+import {BrowserRouter, Link, Route, Routes, useNavigate} from "react-router-dom";
+import ProductPage from "../ProductPage/index.jsx";
+import product from "../Product/index.jsx";
+import Product from "../Product/index.jsx";
+const ProductContainer = ({id, category, title, price, image, description,product}) => {
+    const navigate = useNavigate()
+    const clickOnImage = () => {
+        navigate(`/product-page/${id}`);
+    };
+
     return (
         <div>
-        <img src={image} alt={image} className='img'/>
+            <Link to={`/product-page/${id}`}>
+        <img src={image} alt={image} className='img' onClick={clickOnImage}/>
+            </Link>
             <br/>
          <div className='product-price'>£{price.toFixed(2)}</div>
             <br/>
