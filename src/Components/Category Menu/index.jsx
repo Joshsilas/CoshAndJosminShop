374e937ffda66c20f9b125b6daf1a1c58dd2cf72
Joshsilas <137796554+Joshsilas@users.noreply.github.com>
2024-01-15 12:09:45 +0000

    Headway on the category menu css

--- src/Components/Category Menu/index.jsx
+++ src/Components/Category Menu/index.jsx
@@ -26,24 +26,24 @@ const CategoryMenu = ({ text, category }) => {
     };
 
     return (
-        <>
-            <div className={`category-menu ${isOpen ? 'open' : ''}`}>
-                <button className='catButton' onClick={toggleMenu}>
-                    <img className="barsImage" src='/src/assets/bars-solid.svg' alt="BarsLogo" />
-                    {text}
-                </button>
-                {isOpen && (
-                    <ul>
-                        {categories.map((category, index) => (
-                            <li key={index}>
-                                <Link className="List" to={`/category/${category.toLowerCase()}`}>{category.toUpperCase()}</Link>
-                            </li>
-                        ))}
+        <div className={`category-menu ${isOpen ? 'open' : ''}`}>
+            <button className='catButton' onClick={toggleMenu}>
+                <img className="barsImage" src='/src/assets/bars-solid.svg' alt="BarsLogo" />
+                {text}
+            </button>
+            {isOpen && (
+                <ul className="sidebar-list">
+                    {categories.map((category, index) => (
+                        <li key={index}>
+                            <Link className="List" to={`/category/${category.toLowerCase()}`}>{category.toUpperCase()}</Link>
+                        </li>
+                    ))}
+                    <li>
                         <Link className="List" to="/">HOME PAGE</Link>
-                    </ul>
-                )}
-            </div>
-        </>
+                    </li>
+                </ul>
+            )}
+        </div>
     );
 };
 
