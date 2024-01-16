a892d8cf04a34ce03796fa40c45577103c572251
Joshsilas <137796554+Joshsilas@users.noreply.github.com>
2024-01-16 17:20:14 +0000

    Ok lost the will to live today

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,6 @@ function App() {
     };
 
     useEffect(() => {
-        // Fetch products when the component mounts
         const fetchProducts = async () => {
             try {
                 const response = await fetch('https://fakestoreapi.com/products');
