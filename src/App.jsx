78d893b40ddafde2e3665788c6ac8c73f6d91c21
Joshsilas <137796554+Joshsilas@users.noreply.github.com>
2024-01-16 15:18:44 +0000

    The search bar is kind of doing what I want but every type now makes the prodcut page move and rerender.

--- src/App.jsx
+++ src/App.jsx
@@ -18,17 +18,17 @@ function App() {
     };
 
     useEffect(() => {
-        const fetchProducts = async () => {
+        const fetchSearch= async () => {
             try {
                 const response = await fetch('https://fakestoreapi.com/products');
-                const products = await response.json();
-                setData(products);
+                const search = await response.json();
+                setData(search);
             } catch (err) {
                 console.log(err);
             }
         };
 
-        fetchProducts();
+        fetchSearch();
     }, []);
 
     return (
