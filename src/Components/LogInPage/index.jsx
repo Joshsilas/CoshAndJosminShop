52fdb01069baeebb5b1514556c3b7fff60c2cbab
Joshsilas <137796554+Joshsilas@users.noreply.github.com>
2024-01-24 12:25:34 +0000

    baby steps

--- src/Components/LogInPage/index.jsx
+++ src/Components/LogInPage/index.jsx
@@ -1,12 +1,15 @@
 import './LogInPage.css'
 import InputForms from "../InputForms/index.jsx";
+import Button from "../Button/index.jsx";
+import React from "react";
 
 const LogInPage = () => {
     return (
         <>
-            <p>LOG IN HERE MOTHER PLUM</p>
-            <InputForms className="logInforms" placeholder={"username"}/>
-            <InputForms className="logInforms" placeholder={"password"}/>
+            <p className="signInMessage">Please enter your email and password to sign in</p>
+            <InputForms className="logInforms" placeholder={"Email address"}/>
+            <InputForms className="logInforms" placeholder={"Password"}/>
+            <Button className="signInButton" text="Sign In" />
         </>
     )
 }
