334042a7d285ba527e0bf1edd1491152089430f9
Joshsilas <137796554+Joshsilas@users.noreply.github.com>
2024-02-12 14:53:40 +0000

    good headway

--- src/Components/CartPage/index.jsx
+++ src/Components/CartPage/index.jsx
@@ -1,23 +1,36 @@
 import React, {useState} from "react";
 import './Cartpage.css';
+import Button from "../Button/index.jsx";
+import {Link} from "react-router-dom";
 const CartPage = ({ cartProducts, removeFromCart }) => {
 
     return (
         <div>
-            <h1>Cart Page</h1>
-
-            {cartProducts.map((product, index,) => (
-                <div key={index} className="cart-product">
-                    <img className="cartproductImage" src={product.image}></img>
-                    <div className="cart-product-details">
-                        <h3>{product.title}</h3>
-                        <p>Price: £{product.price.toFixed(2 ) * product.quantity}</p>
-                        <p>Quantity: {product.quantity}</p>
-                        <button type='button' className='btn' onClick={() => removeFromCart(product.id)}>Remove product</button>
-                    </div>
+            <h1 className="categoryBanner">Cart Page</h1>
+            {cartProducts.length === 0 ? (
+                <div className= "emptyCartMessage">
+                <p className="emptyCartMessage">Your cart is currently empty.</p>
+                <Link className="backToHomePageLinkCartpage" to="/" >
+                    Back to home page
+                </Link>
                 </div>
-            ))}
+            ) : (
+                <div className="cart-layout">
+                    {cartProducts.map((product, index) => (
+                        <div key={index} className="cart-product">
+                            <img className="cartproductImage" src={product.image} alt={`Product: ${product.title}`} />
+                            <div className="cart-product-details">
+                                <p className="cartProductTitle">{product.title}</p>
+                                <p className="cartProductPrice">£{(product.price * product.quantity).toFixed(2)}</p>
+                                <p className="cartProductQuanity">Quantity: {product.quantity}</p>
+                                <Button type='button' className='removeButton' onClick={() => removeFromCart(product.id)} text="Remove product" />
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            )}
         </div>
     );
 }
-export default CartPage
+
+export default CartPage;
