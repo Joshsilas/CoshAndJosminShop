05d967e63e2ec0f6434f4c5720025627b046dfbb
Joshsilas <137796554+Joshsilas@users.noreply.github.com>
2024-02-15 19:36:38 +0000

    fixed a bug where pushing enter in a form reshreshed the page

--- src/Components/InputForms/index.jsx
+++ src/Components/InputForms/index.jsx
@@ -3,10 +3,15 @@ import React from "react";
 const InputForms = ({ placeholder, className, value, onChange, type, id, onSubmit  }) => {
     const handleChange = (e) => {
         onChange && onChange(e.target.value);
-    };
+        };
+
+    const handleSubmit = (event) => {
+    event.preventDefault();
+    }
+
     return (
         <>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input
                     id={id}
                     className={className}
